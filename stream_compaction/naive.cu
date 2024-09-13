47dd28580940ce69f588348dc7ea2d342b706508
a1yssan13 <niealyssa@gmail.com>
2024-09-13 11:49:19 -0400

    wip: naive gpu implementation

--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -3,6 +3,8 @@
 #include "common.h"
 #include "naive.h"
 
+#define blockSize 256
+
 namespace StreamCompaction {
     namespace Naive {
         using StreamCompaction::Common::PerformanceTimer;
@@ -12,13 +14,39 @@ namespace StreamCompaction {
             return timer;
         }
         // TODO: __global__
-
+        __global__ void naiveScanKernel(int n, int d, int *odata, const int *idata) {
+            int index = threadIdx.x + blockIdx.x * blockDim.x;
+            if (index < n) {
+                if (index >= pow(2, d)) {
+                    odata[index] = idata[index - pow(2, d-1)] + idata[index];
+                } else {
+                    odata[index] = idata[index];
+                }
+            }
+        }
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
-            // TODO
+            int *dev_idata;
+            int *dev_odata;
+
+            dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize); 
+
+            cudaMalloc((void**)&dev_idata, n * sizeof(int));
+            cudaMalloc((void**)&dev_odata, n * sizeof(int));
+            cudaMemcpy(dev_idata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+            
+            for (int d = 1; d <= ilog2ceil(n); d++) {
+                naiveScanKernel<<<fullBlocksPerGrid, blockSize>>>(n, d, dev_odata, dev_idata);
+                swap(dev_idata, dev_odata);
+            }
+            
+            cudaMemcpy(odata, dev_odata, n * sizeof(int), cudaMemcpyDeviceToHost);
+            cudaFree(dev_idata);
+            cudaFree(dev_odata);
+
             timer().endGpuTimer();
         }
     }
